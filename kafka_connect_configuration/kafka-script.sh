72d0c823d9734038be9842de027c096a5d682f44
Fouad Aniq <46678564+psisco2@users.noreply.github.com>
2022-12-02 17:45:50 +0100

    Delete kafka_connect_configuration directory

--- kafka_connect_configuration/kafka-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sleep 15.0s
-
-usr/bin/kafka-topics --bootstrap-server "$BOOTSTRAP_SERVER" --topic "$KAFKA_TOPIC" --create --partitions 1 --replication-factor 1
-
-usr/bin/kafka-console-producer --bootstrap-server "$BOOTSTRAP_SERVER" --topic "$KAFKA_TOPIC"
-{"name":"jhon","age":30}
